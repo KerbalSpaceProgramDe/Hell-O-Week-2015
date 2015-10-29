e2eeffd76d02ec38f51aeb5ed52685f77a42e5d1
Thomas <dorianstoll@googlemail.com>
2015-10-29 16:17:25 +0100

    Code

---
+++ Source/KCST.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * Hell-O-Week 2015
+ * kerbalspaceproram.de
+ */
+
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace KCST
+{
+    /// Einige Überraschungen für die Hell-O-Week 2015
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class KerbalCenterForScreamsAndTorture : MonoBehaviour
+    {
+        void Start()
+        {
+            /// Main-Menu: Den Mond rot machen
+            CelestialBody mun = PSystemManager.Instance.localBodies.Find(b => b.name == "Mun");
+            if (mun != null)
+            {
+                PQSMod_VertexBloodColor red = new GameObject("VertexBloodColor").AddComponent<PQSMod_VertexBloodColor>();
+                red.transform.parent = mun.pqsController.transform;
+                red.order = Int32.MaxValue;
+                red.modEnabled = true;
+                mun.scaledBody.renderer.material.mainTexture = GameDatabase.Instance.GetTexture("Hell-o-Week 2015/Textures/mun_color", false);
+            }
+        }
+    }
+
+    /// Blutmond-PQSMod
+    public class PQSMod_VertexBloodColor : PQSMod
+    {
+        /// Amount of red color
+        public Color32 red = new Color32(138, 7, 7, 255);
+
+        public override void OnVertexBuild(PQS.VertexBuildData data)
+        {
+            data.vertColor *= red;
+        }
+    }
+}
---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hell-O-Week-2015")]
+[assembly: AssemblyDescription("Hell-O-Week 2015 | kerbalspaceprogram.de")]
+[assembly: AssemblyCompany("kerbalspaceprogram.de")]
+[assembly: AssemblyProduct("Hell-O-Week-2015")]
+[assembly: AssemblyCopyright("Copyright © kerbalspaceprogram.de 2015")]
+[assembly: ComVisible(false)]
+[assembly: Guid("33b33e3d-21e9-4c78-a44c-afb9dd44becc")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: KSPAssembly("Hell-O-Week", 2015, 0)]
